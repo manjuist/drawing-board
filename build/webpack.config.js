ec74b69834c35f17481f7a69df2cae0a18935082
Deve <deve_m@163.com>
2019-06-08 23:43:49 +0800

    style

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -37,6 +37,15 @@ module.exports = {
         },
         'css-loader',
         'sass-loader',
+        {
+          loader:'sass-resources-loader',
+            options : {
+                resources: [
+                    path.join(ROOT_PATH, 'src/common/styles/_variable.scss'),
+                ]
+            }
+        }
+
       ],
     },{
       test: /\.(woff|woff2|otf|ttf|eot|svg)$/,
--- src/components/dock/index.tsx
+++ src/components/dock/index.tsx
@@ -2,19 +2,16 @@ import React from 'react'
 import List from './List'
 import './dock.scss'
 
-function Dock(){
-  return (
-    <List
-      className="dock"
-      data={[
+const data = [
         {name:'画笔', icon:'pen'},
         {name:'图片', icon:'picture'},
         {name:'橡皮', icon:'eraser'},
         {name:'套索', icon:'lasso'},
         {name:'颜色', icon:'color'},
-      ]}
-  />
-  )
+      ]
+
+function Dock(){
+  return ( <List className="dock" data={data} />)
 }
 
 export default Dock
