75b00e256394259fa988cf98d9d2e8df4ef81b68
dewei.ma <dewei.ma@lvyuetravel.com>
2020-11-03 20:38:46 +0800

    feat(eslint): eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,31 +2,25 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
   },
-  extends: [ 
-    'plugin:react/recommended',
+  extends: [
     'airbnb',
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaFeatures: { jsx: true, },
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  settings: {
-      'import/resolver': {
-          'webpack': {
-              config:'./build/webpack.config.js'
-          }
-      }
-  },
   plugins: [
     'react',
-    '@typescript-eslint',
-  ]
+  ],
+  rules: {
+  },
+  parser: '@typescript-eslint/parser'
 };
